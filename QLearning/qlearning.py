91ccb48fa8607cfb48b291f4f4cdbcbb04b4523e
aelitashen <aelitashen@gmail.com>
2020-11-07 17:06:59 -0800

    Update qlearning.py

--- QLearning/qlearning.py
+++ QLearning/qlearning.py
@@ -1,10 +1,11 @@
 import numpy as np
 import copy
 import gym
-from gym_gomoku.envs import utiol
+from gym_gomoku.envs import util
 from gym_gomoku.envs import gomoku
 
 def train(num_episode, alpha, gamma, epsilon):
+    q_table = np.zeros([env.observation_space.n, env.action_space.n])
     for i in range(num_episode):
         state = env.reset()
         reward = 0
@@ -24,30 +25,46 @@ def train(num_episode, alpha, gamma, epsilon):
     
     if (episode+1) % 100 == 0:
         env.render()
-
-def evaluate(state, num_episode, epoch, penalty, reward):
-    terminated = False
-    while not terminated:
-        action = np.argmax(q_table[state])
-        state, reward, terminated, info = enviroment.step(action)
-        if reward < 0:
-            penalty += 1
-        epoch += 1
+    return q_table
 
 
 def main():
     env = gym.make('Gomoku7x7-v0')
-    _, state = env.reset()
-    num_episode = 1000
+    num_episode = 10000
     alpha = 0.1
     gamma = 0.6
     epsilon = 0.1
-    q_table = np.zeros([env.observation_space.n, env.action_space.n])
-    train(num_episode, alpha, gamma, epsilon)
-    done = False
-    while not done:
-        env.render('human')
-        
+    q_table = train(num_episode, alpha, gamma, epsilon)
+
+    total_epochs = 0
+    total_penalties = 0
+    eval_episodes = 100
+
+    # Evaluation
+    for i in range(eval_episodes):
+        _, state = env.reset()
+        epochs = 0
+        penalties = 0
+        reward = 0
+        terminated = False
+
+        while not terminated:
+            env.render('human')
+            action = np.argmax(q_table[state])
+            state, reward, terminated, info = environment.step(action)
+
+            if reward == -1:
+                penalties += 1
+            
+            epochs += 1
+
+        total_epochs += epochs
+        total_penalties += penalties
+    
+    print("Results")
+    print("**********************************")
+    print("Epochs per episode: {}".format(total_epochs / eval_episodes))
+    print("Penalties per episode: {}".format(total_penalties / eval_episodes))
 
 if __name__ == '__main__':
     main()
